c8fdfc50b0db811079f71a50ed113a1a1f6fc0fc
gmcln-rct <producersama@gmail.com>
2024-03-11 20:46:10 -0400

    Work on card styling

--- app/components/card.js
+++ app/components/card.js
@@ -5,20 +5,24 @@ import Image from "next/image";
 import styles from "./card.module.scss";
 
 export default function Card({ photo, title, text, slug }) {
-  function truncateWithEllipses(text, max) {
-    return text.substr(0, max - 1);
-  }
 
-  const truncatedText = truncateWithEllipses(text, 100);
+    function truncateBasedOnWordCount(text, max) {
+        return text.split(" ").slice(0, max).join(" ");
+    }
+//   function truncateWithEllipses(text, max) {
+//     return text.substr(0, max - 1);
+//   }
+
+  const truncatedText = truncateBasedOnWordCount(text, 20);
   return (
     <div className={styles.card}>
-      <Image src={photo} alt="Card Image" width={300} height={200} />
-      <div className="card-content">
+      <Image className={styles.card__image} src={photo} alt="Card Image" width={300} height={200} />
+      <div className={styles.card__content}>
         <p>{truncatedText}...</p>
-        <Link href={`/details/${slug}`}>
-          <button>More</button>
-        </Link>
       </div>
+        <Link className={styles.card__button} href={`/success/${slug}`}>
+          More
+        </Link>
     </div>
   );
 }
