c7a83018831981b00339294406c148bd3d4e1757
gmcln-rct <producersama@gmail.com>
2024-03-12 20:16:22 -0400

    Work on dynamic page

--- app/success/[id]/page.js
+++ app/success/[id]/page.js
@@ -1,19 +1,19 @@
-import {successStories} from '../../data/success-stories';
+import { successStories } from "../../data/success-stories";
 
-export default function IndividualStory({ params}) {
-
+export default function IndividualStory({ params }) {
   function findStory(id) {
-    return successStories.find(story => story.id === id);
+    return successStories.find((story) => story.id === id);
   }
 
   const story = findStory(params.id);
 
   return (
     <>
-    <div>IndividualStory</div>
-    <div>{params.id}</div>
-    <h2>{story.title}</h2>
-    <p>{story.description}</p>
+      <div>IndividualStory</div>
+      <div>{params.id}</div>
+      <h2>{story.title}</h2>
+      <p>{story.description}</p>
+      <div>{story.longTextRte}</div>
     </>
-  )
+  );
 }
