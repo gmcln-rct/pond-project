5259ab0d8c37e5417c4485450093f0a0b43df6b7
gmcln-rct <producersama@gmail.com>
2024-03-12 20:34:29 -0400

    update rte

--- app/success/[id]/page.js
+++ app/success/[id]/page.js
@@ -1,5 +1,7 @@
 import { successStories } from "../../data/success-stories";
 
+import styles from "./page.module.scss";
+
 export default function IndividualStory({ params }) {
   function findStory(id) {
     return successStories.find((story) => story.id === id);
@@ -8,12 +10,12 @@ export default function IndividualStory({ params }) {
   const story = findStory(params.id);
 
   return (
-    <>
+    <section className={styles.successStory}>
       <div>IndividualStory</div>
       <div>{params.id}</div>
       <h2>{story.title}</h2>
       <p>{story.description}</p>
       <div>{story.longTextRte}</div>
-    </>
+    </section>
   );
 }
