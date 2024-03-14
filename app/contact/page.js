f6546eb207538270e867dc14c94434278abec951
gmcln-rct <producersama@gmail.com>
2024-03-13 21:11:25 -0400

    Work on styling

--- app/contact/page.js
+++ app/contact/page.js
@@ -6,27 +6,6 @@ export default function ContactPage() {
   return (
     <section className={styles.contact}>
       <ContactForm />
-    {/* <form className={styles.contact__form}>
-      <h1>Contact Us</h1>
-      <label className={styles.contact__label}>
-        Name:
-        <input className={styles.contact__input}type="text" name="name" />
-      </label>
-      <label className={styles.contact__label}>
-        Email:
-        <input className={styles.contact__input}type="text" name="email" />
-      </label>
-      <label className={styles.contact__label}>
-        Phone:
-        <input className={styles.contact__input}type="number" name="phone" />
-      </label>
-
-      <label className={styles.contact__label}>
-        Message:
-        <textarea name="message" />
-        </label>
-        <input className={styles.contact__button}type="submit" value="Submit" />
-    </form> */}
     </section>
   )
 }
