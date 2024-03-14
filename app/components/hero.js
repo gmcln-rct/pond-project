9867dd047035af500db87abb6ff10ddeedf549cc
gmcln-rct <producersama@gmail.com>
2024-03-13 20:28:13 -0400

    Work on bgvideo

--- app/components/hero.js
+++ app/components/hero.js
@@ -1,19 +1,31 @@
 import styles from "./hero.module.scss";
 
-import Image from 'next/image'
+
 export default function Hero() {
+  const bgVideo = "/videos/pondsample.mp4";
   return (
     <section className={styles.hero}>
-        {/* <Image src="/images/pond-hero.jpg" alt="Pond Hero" className={styles.hero__image} width={900} height={200} /> */}
-        {/* <div className={styles.hero__image}>
-            <Image src="/images/pond-hero.jpg" alt="Pond Hero" width={900} height={200} />
-        </div> */}
-        <div className={styles.hero__content}>
-            <h1 className={styles.hero__title}>Expert advice for pond & lake management.</h1>
-            <p className={styles.hero__subtitle}>Professional environmental consulting for the unique needs of your pond or lake.</p>
-            <p className={styles.hero__subtitle}>Serving New York & New Jersey.</p>
-            <p className={styles.hero__subtitle}>Call us at 914-867-5309</p>
-        </div>
+      <video  src={bgVideo}
+        type="video/mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        className={styles.hero__video}
+         >
+        Your browser does not support the video tag.
+      </video>
+      <div className={styles.hero__content}>
+        <h1 className={styles.hero__title}>
+          Expert advice for pond & lake management.
+        </h1>
+        <p className={styles.hero__subtitle}>
+          Professional environmental consulting for the unique needs of your
+          pond or lake.
+        </p>
+        <p className={styles.hero__subtitle}>Serving New York & New Jersey.</p>
+        <p className={styles.hero__subtitle}>Call us at 914-867-5309</p>
+      </div>
     </section>
-  )
+  );
 }
