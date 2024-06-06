af08fa050198c0041c42589d32b4785933e127b9
gmcln-rct <producersama@gmail.com>
2024-06-05 23:10:54 -0400

    Work on labels

--- app/data/labels.js
+++ app/data/labels.js
@@ -29,4 +29,84 @@ export const labels = [
     height: 386,
     pdfUrl: "/pdfs/procellacorec_nysproductlabel.pdf",
   },
+  {
+    id: "procellacor-ec-sln",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/procellacor-ec-sln.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/procellacor-ec-sln.pdf",
+  },
+  {
+    id: "seclearlabel",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/seclearlabel.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/seclearlabel.pdf",
+  },
+  {
+    id: "komeen_nysproductlabel",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/komeen_nysproductlabel.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/komeen_nysproductlabel.pdf",
+  },
+  {
+    id: "sonarq_nyslabel",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/sonarq_nyslabel.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/sonarq_nyslabel.pdf",
+  },
+  {
+    id: "seclear-sln",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/seclear-sln.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/seclear-sln.pdf",
+  },
+  {
+    id: "harpoongranularnyslabel",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/harpoongranularnyslabel.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/harpoongranularnyslabel.pdf",
+  },
+  {
+    id: "sonar-h4c-slnny-150003",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/sonar-h4c-slnny-150003.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/sonar-h4c-slnny-150003.pdf",
+  },
+  {
+    id: "nys-sonar-h4c-label",
+    title: "ProcellaCOR® EC Product Label",
+    description:
+      "ProcellaCOR EC is a selective herbicide that is designed to control a wide range of aquatic weeds and algae.",
+    image: "/pdfs/thumbs/nys-sonar-h4c-label.jpg",
+    width: 366,
+    height: 386,
+    pdfUrl: "/pdfs/nys-sonar-h4c-label.pdf",
+  },
 ];
