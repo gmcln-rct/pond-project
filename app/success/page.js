7fa955471e02ed9d8a35446b34f83cf312958ad1
gmcln-rct <producersama@gmail.com>
2024-06-06 21:40:12 -0400

    Update html

--- app/success/page.js
+++ app/success/page.js
@@ -6,9 +6,9 @@ import { successStories } from "../data/success-stories";
 
 export default function SuccessPage() {
   return (
-    <section className={styles.services}>
-      <h1 className={styles.services__header}>Success Stories</h1>
-      <p className={styles.services__subheader}>
+    <section className={styles.success}>
+      <h1 className={styles.success__header}>Success Stories</h1>
+      <p className={styles.success__subheader}>
         Little Bear has achieved successful results on a variety of projects
         throughout New York and New Jersey.
       </p>
