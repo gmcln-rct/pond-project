6f90abd4d9e0a22038cd1296e055bbf01888a3ae
gmcln-rct <producersama@gmail.com>
2024-06-09 10:39:32 -0400

    Update success

--- app/success/[id]/page.js
+++ app/success/[id]/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import { successStories } from "../../data/success-stories";
 
-import styles from "../page.module.scss";
+import styles from "./page.module.scss";
 
 export default function IndividualStory({ params }) {
   function findStory(id) {
@@ -13,16 +13,18 @@ export default function IndividualStory({ params }) {
 
   return (
     <section className={styles.successStory}>
-      <div className={styles.successStory__wrapper}>
-        <Image
+      <div className={styles.successStory__imageWrapper}>
+      <Image
           className={styles.successStory__image}
           src={story.image}
           alt={story.title}
-          width={507}
-          height={446}
+          width={1500}
+          height={1000}
           // layout="responsive"
         />
       </div>
+      <div className={styles.successStory__contentWrapper}>
+
       <div className={styles.successStory__content}>
         <h2 className={styles.successStory__title}>{story.title}</h2>
         <p className={styles.successStory__client}>Client: {story.client}</p>
@@ -31,6 +33,7 @@ export default function IndividualStory({ params }) {
           className={styles.successStory__content}
           dangerouslySetInnerHTML={{ __html: story.longTextRte }}
         ></div>
+      </div>
       </div>
     </section>
   );
