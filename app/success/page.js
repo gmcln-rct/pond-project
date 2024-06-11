d78864f01c1e5074d363f865ecded3f6be72a689
gmcln-rct <producersama@gmail.com>
2024-06-10 20:17:10 -0400

    Update to video bg

--- app/success/page.js
+++ app/success/page.js
@@ -1,19 +1,25 @@
 import styles from "./page.module.scss";
-
 import Cards from "../components/cards";
-
 import { successStories } from "../data/success-stories";
 
 export default function SuccessPage() {
   return (
     <section className={styles.success}>
-      <h1 className={styles.success__header}>Success Stories</h1>
-      <p className={styles.success__subheader}>
-        Little Bear has achieved successful results on a variety of projects
-        throughout New York and New Jersey.
-      </p>
+      <div className={styles.success__videoBackground}>
+        <video autoPlay muted loop playsInline>
+          <source src="/videos/pond-bg-1.mp4" type="video/mp4" />
+          <img src="/images/pond-bg-temp.jpg" alt="Fallback Image" />
+        </video>
+      </div>
+      <div className={styles.success__content}>
+        <h1 className={styles.success__header}>Success Stories</h1>
+        <p className={styles.success__subheader}>
+          Little Bear has achieved successful results on a variety of projects
+          throughout New York and New Jersey.
+        </p>
 
-      <Cards cardcontent={successStories} />
+        <Cards  cardcontent={successStories} />
+      </div>
     </section>
   );
 }
