1836afb22cfcebaa683d2ce612ee36393e900115
gmcln-rct <producersama@gmail.com>
2024-06-19 14:51:52 -0400

    Work on hero

--- app/services/page.js
+++ app/services/page.js
@@ -9,6 +9,12 @@ export default function ServicesPage() {
 
   return (
     <section className={styles.services}>
+      <div className={styles.services__videoBackground}>
+        <video autoPlay muted loop playsInline>
+          <source src="/videos/pond-bg-3.mp4" type="video/mp4" />
+          <img src="/images/pond-bg-temp.jpg" alt="Fallback Image" />
+        </video>
+      </div>
       <h1 className={styles.services__header}>Our Services</h1>
       <p className={styles.services__subheader}>
         We provide a variety of enviornmental services:
