84304c94883c24842860f0c8972ade1e61de7ef9
gmcln-rct <producersama@gmail.com>
2024-06-23 17:16:30 -0400

    Update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      EMAIL_USER: process.env.EMAIL_USER,
+      EMAIL_PASS: process.env.EMAIL_PASS,
+    },
+    // Your existing configurations (if any) go here
+  };
+  
+  export default nextConfig;
