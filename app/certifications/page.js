12db4da724e0f5118682a396e4b4c0d5faadb74c
gmcln-rct <producersama@gmail.com>
2024-07-06 19:03:54 -0400

    Update styling

--- app/certifications/page.js
+++ app/certifications/page.js
@@ -1,16 +1,21 @@
-
-
 import InfoContainer from "../components/info-container";
 
 import { certificationData } from "../data/certifications";
+
+import styles from "./page.module.scss";
 
 const certificationStyleClass = "certification";
 
 export default function Certifications() {
   return (
     <section className={certificationStyleClass}>
-        <InfoContainer infoUnitsData={certificationData}/>
+      <div className={styles.certificationsPage__header}>
+        <h1>Certifications</h1>
+        <p className={styles.certificationsPage__description}>
+          Little Bear has certifications and partnerships with a range of environmental agencies and organizations.
+        </p>
+      </div>
+      <InfoContainer infoUnitsData={certificationData} />
     </section>
   );
 }
-
