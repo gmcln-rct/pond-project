b11c98e86ea2ea28d9b60cd68b77653f1cf12b3e
gmcln-rct <producersama@gmail.com>
2024-07-17 13:20:08 -0400

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,54 @@
+export default function sitemap() {
+    const baseUrl = process.env.BASE_URL;
+    // const baseUrl = 'https://littlebearenvironmental.com/';
+    return [
+      {
+        url: baseUrl,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 1,
+      },
+      {
+        url: `${baseUrl}/contact`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: `${baseUrl}/services`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: `${baseUrl}/success`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: `${baseUrl}/certifications`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.5,
+      },
+      {
+        url: `${baseUrl}/labels`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.5,
+      },
+      {
+        url: `${baseUrl}/about/littlebear`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.2,
+      },
+      {
+        url: `${baseUrl}/about/site`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.2,
+      }
+    ]
+  }
