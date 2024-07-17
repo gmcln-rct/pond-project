43a40270bb6ec426f7cd981ea11e989a965b5c5d
gmcln-rct <producersama@gmail.com>
2024-07-17 14:03:49 -0400

    Work on ga tags

--- app/layout.js
+++ app/layout.js
@@ -6,11 +6,17 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 
 export const metadata = {
-  title: "Little Bear Environmental | Environmental consulting serving New York & New Jersey",
+  title:
+    "Little Bear Environmental | Environmental consultants for New York & New Jersey",
   template: "%s | Little Bear Environmental",
   description:
     "Little Bear Environmental provides a variety of natural resource services for municipalities, non-profit organizations, and private landowners.",
 };
+
+import Script from "next/script";
+
+const gaTag = process.env.GOOGLE_ANALYTICS;
+const gaScript = "https://www.googletagmanager.com/gtag/js?id=" + gaTag;
 
 export default function RootLayout({ children }) {
   return (
@@ -20,8 +26,10 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="robots" content="noindex, nofollow" />
-
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        ></link>
         <meta name="author" content="Little Bear Environmental" />
         <meta
           name="keywords"
@@ -33,47 +41,34 @@ export default function RootLayout({ children }) {
           New York, New Jersey, NY, NJ, Westchester County, Long Island, Hudson Valley, North Jersey, Central Jersey"
         />
         <meta name="application-name" content="Little Bear Environmental" />
-        <meta name="apple-mobile-web-app-title" content="Little Bear Environmental" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Little Bear Environmental"
+        />
         <meta name="msapplication-TileColor" content="#5e376c" />
         <meta name="theme-color" content="#5e376c" />
-
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://Littlebearenvironmental.com" />
+        {/* <meta property="og:url" content="https://Littlebearenvironmental.com" /> */}
         <meta property="og:site_name" content="Little Bear Environmental" />
         <meta property="og:locale" content="en_US" />
         <meta
           property="og:image"
           content="../public/images/social/pond-og-image.jpg"
-        />`
+        />
 
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
         <link
           rel="apple-touch-icon"
           href="/apple-icon?<generated>"
           type="image/<generated>"
           sizes="<generated>"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
         <link rel="manifest" href="/site.webmanifest"></link>
+        {/* Google Analytics */}
       </Head>
+      {/* <Script src={gaScript}></Script> */}
       <body className="main-layout">
         <Header />
         <div className="content-wrapper">
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,54 +1,55 @@
 export default function sitemap() {
     const baseUrl = process.env.BASE_URL;
+    console.log('baseUrl', baseUrl);
     // const baseUrl = 'https://littlebearenvironmental.com/';
     return [
-      {
-        url: baseUrl,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 1,
-      },
-      {
-        url: `${baseUrl}/contact`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: `${baseUrl}/services`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: `${baseUrl}/success`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: `${baseUrl}/certifications`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.5,
-      },
-      {
-        url: `${baseUrl}/labels`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.5,
-      },
-      {
-        url: `${baseUrl}/about/littlebear`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.2,
-      },
-      {
-        url: `${baseUrl}/about/site`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.2,
-      }
+    //   {
+    //     url: baseUrl,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 1,
+    //   },
+    //   {
+    //     url: `${baseUrl}/contact`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.8,
+    //   },
+    //   {
+    //     url: `${baseUrl}/services`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.8,
+    //   },
+    //   {
+    //     url: `${baseUrl}/success`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.8,
+    //   },
+    //   {
+    //     url: `${baseUrl}/certifications`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.5,
+    //   },
+    //   {
+    //     url: `${baseUrl}/labels`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.5,
+    //   },
+    //   {
+    //     url: `${baseUrl}/about/littlebear`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.2,
+    //   },
+    //   {
+    //     url: `${baseUrl}/about/site`,
+    //     lastModified: new Date(),
+    //     changeFrequency: 'monthly',
+    //     priority: 0.2,
+    //   }
     ]
   }
