4c99a79dd65a55591330975c2d0af47cee04f2c7
gmcln-rct <producersama@gmail.com>
2024-07-25 22:14:39 -0400

    Try to fix unsubscribe 3

--- app/unsubscribe/page.js
+++ app/unsubscribe/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Suspense } from 'react';
 
 import styles from './page.module.scss';
